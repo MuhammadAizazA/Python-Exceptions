1a6e567ead780e16cbc82a1f913efb8af226989f
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-20 10:57:50 +0500

    Try, except, else and finally Practiced

--- src/module_1.py
+++ src/module_1.py
@@ -1,5 +1,14 @@
-def check_package_working():
-    print("Package is working")
+def divide_numbers(numerator,denominator):
+    try:
+        result=numerator/denominator
+    except ZeroDivisionError as e:
+        print("Denominator cannot be 0 \nError:",e)
+    except TypeError as e:
+        print("Numerator and Denominator can only be Integers \nError:",e)
+    else:
+        print(f"{numerator}/{denominator}={result}")
+    finally:
+        print("Divide Function Execution Completed!\n")
     
 if __name__=='__main__':
-    check_package_working()
+    divide_numbers(10,2)
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,9 @@
 from src import module_1 as m1
 
 if __name__=="__main__":
-    m1.check_package_working()
+    m1.divide_numbers(10, 2)
+    m1.divide_numbers(5, 0)
+    m1.divide_numbers("abc", 2)    
+    m1.divide_numbers(8, "def")    
+    m1.divide_numbers(15, 3)       
 
