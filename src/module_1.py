6218aa05ceb5b9aa82f7238fdd10dd4b07f618ec
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-21 18:59:23 +0500

    tried nested nested exceptions

--- src/module_1.py
+++ src/module_1.py
@@ -1,14 +1,27 @@
-def divide_numbers(numerator,denominator):
+def nested_diveide_numbers(n,m):
     try:
-        result=numerator/denominator
+        result=n/m     
     except ZeroDivisionError as e:
         print("Denominator cannot be 0 \nError:",e)
     except TypeError as e:
         print("Numerator and Denominator can only be Integers \nError:",e)
     else:
-        print(f"{numerator}/{denominator}={result}")
+        print(f"{n}/{m}={result}")
     finally:
         print("Divide Function Execution Completed!\n")
-    
+
+def divide_numbers(numerator,denominator):
+    try:
+        result=numerator/denominator
+        nested_diveide_numbers("7",3)     
+    except ZeroDivisionError as e:
+        print("nested_Denominator cannot be 0 \nError:",e)
+    except TypeError as e:
+        print("nested_Numerator and Denominator can only be Integers \nnested_Error:",e)
+    else:
+        print(f"nested_{numerator}/{denominator}={result}")
+    finally:
+        print("nested_Divide Function Execution Completed!\n")
+
 if __name__=='__main__':
-    divide_numbers(10,2)
+    divide_numbers("5",4)
